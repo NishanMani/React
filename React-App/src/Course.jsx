93c0ea23e2f6985dab0b7f4259e43c8d04875c30
Nishan K M <nishan.km@knackforge.com>
2026-01-08 14:37:09 +0530

    hook

--- React-App/src/Course.jsx
+++ React-App/src/Course.jsx
@@ -1,14 +1,35 @@
-function Course({ name, sentence, img }) {
+import { useEffect, useState } from "react";
 
-    function BuyCourse(){
-        console.log(purchased)
-    }
+function Course({ name, sentence, price, img, onDelete }) {
+  const [purchased, setPurchased] = useState(false);
+  const [discount, setDiscount] = useState(price);
+
+  function buyCourse(amt) {
+    setPurchased(true);
+    setDiscount(prev => prev - amt);
+  }
+
+  useEffect(() => {
+    console.log('inside course use effect')})
+
   return (
     <div className="card">
-      <img src={img} alt="course" />
+      <img src={img} alt={name} />
       <h3>{name}</h3>
       <p>{sentence}</p>
-      <button onclick="">Buy </button>
+      <h4>Price: ₹{discount}</h4>
+
+      <button onClick={() => buyCourse(20)}>
+        Buy Now
+      </button>
+
+      <button onClick={onDelete}>
+        Delete
+      </button>
+
+      <p>
+        {purchased ? "Already Purchased" : "Get it Now!"}
+      </p>
     </div>
   );
 }
--- React-App/src/CourseList.jsx
+++ React-App/src/CourseList.jsx
@@ -1,23 +1,33 @@
-import Course from './Course';
-import HTML from './assets/ss.png';
+import { useState, useEffect } from "react";
+import Course from "./Course";
+import HTML from "./assets/ss.png";
 
 function CourseList() {
-  const courses = [
-    { name: "HTML", sentence: "HTML course by code io", img: HTML },
-    { name: "CSS", sentence: "CSS course by code io", img: HTML },
-    { name: "JS", sentence: "JS course by code io", img: HTML }
-  ];
+  const [courses, setCourses] = useState([
+    { id: 1, name: "HTML", sentence: "HTML course by code io", price: 100, img: HTML },
+    { id: 2, name: "CSS", sentence: "CSS course by code io", price: 200, img: HTML },
+    { id: 3, name: "JS", sentence: "JS course by code io", price: 300, img: HTML }
+  ]);
+  useEffect(() => {
+    console.log('use Effect called');
+});
 
-  
+  function handleDelete(id) {
+    setCourses(prevCourses =>
+      prevCourses.filter(course => course.id !== id)
+    );
+  }
 
   return (
     <>
-      {courses.map((course, index) => (
+      {courses.map(course => (
         <Course
-          key={index}
+          key={course.id}
           name={course.name}
           sentence={course.sentence}
           img={course.img}
+          price={course.price}
+          onDelete={() => handleDelete(course.id)}
         />
       ))}
     </>
